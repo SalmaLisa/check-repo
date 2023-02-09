e059c5de6c459fb630b818491ed7d423fe4541aa
sami <samixp@live.com>
2023-02-10 00:28:00 +0100

    ...

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,7 +4,7 @@ import logger from './middleware/logger';
 import toast from './middleware/toastify';
 import reducer from './reducer';
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function () {
+export default function configurestore () {
   return configureStore({
     reducer,
     middleware: [
